391e14bd7683dbc51efab19f54da2706bb91d65c
KANAK <119925670+Kanak0983@users.noreply.github.com>
2023-10-31 23:39:10 +0530

    Add files via upload

---
+++ DivideInteger.java
@@ -0,0 +1,24 @@
+public class Solution {
+    public int divide(int dividend, int divisor) {
+		//all of these if else statements are only for edge cases and to make both dividend and divisor negative
+        if (dividend == Integer.MIN_VALUE && divisor == -1)
+            return Integer.MAX_VALUE;
+        if (divisor == 1)
+            return dividend;
+        if (dividend > 0 && divisor > 0)
+            return divide(-dividend, -divisor);
+        else if (dividend > 0)
+            return -divide(-dividend, divisor);
+        else if (divisor > 0)
+            return -divide(dividend, -divisor);
+        if (divisor < dividend || dividend == 0)
+            return 0;
+        int sum = divisor;
+        int mult = 1;
+        while ((sum + sum) < sum && (sum + sum) >= dividend) {
+            sum += sum;
+            mult += mult;
+        }
+        return mult + divide(dividend - sum, divisor);
+    }
+}
